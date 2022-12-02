3e52f05744a1425d32b6bdf45d49794e1f900514
VladimirYanovskiy <117820316+VladimirYanovskiy@users.noreply.github.com>
2022-12-02 18:04:30 +0300

    Update Задание 16.9.2.py

--- Задание 16.9.2.py
+++ Задание 16.9.2.py
@@ -6,7 +6,7 @@ class Rectangle:
         self.heigth = heigth
 
     def __str__(self):
-        return f'Rectangle : {self.x}, {self.y}, {self.width}, {self.heigth}.'
+        return f'Rectangle: {self.x}, {self.y}, {self.width}, {self.heigth}.'
 
     def get_area(self):
         return self.width * self.heigth
